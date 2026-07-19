ca221e1a96baf297ffac1de7f62613690179afc6
ilan cooke <ilancooke@users.noreply.github.com>
2026-07-18 18:38:54 -0700

    Initial Commit

---
+++ Monopoly/MonopolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonopolyApp.swift
+//  Monopoly
+//
+//  Created by ilan on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MonopolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
